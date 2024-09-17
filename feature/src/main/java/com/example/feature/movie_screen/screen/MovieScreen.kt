db78e1289b655cf5da05031e9d3fc941be8f6176
BRBX <egorka2005sautkin@gmail.com>
2024-09-17 23:50:03 +0300

    create movie screen header

--- feature/src/main/java/com/example/feature/movie_screen/screen/MovieScreen.kt
+++ feature/src/main/java/com/example/feature/movie_screen/screen/MovieScreen.kt
@@ -1,24 +1,28 @@
 package com.example.feature.movie_screen.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.core.ui.theme.mColors
+import com.example.feature.movie_screen.sections.HeaderSection
 
 @Composable
 fun MovieScreen(
     movieId: Int,
     mainScaffoldPadding: PaddingValues,
-    viewModel: MovieScreenVM
+    viewModel: MovieScreenVM,
 ) {
     val movieDetails = viewModel.movieDetails.collectAsStateWithLifecycle().value
     LaunchedEffect(movieDetails == null) {
@@ -26,19 +30,23 @@ fun MovieScreen(
     }
 
     if(movieDetails != null) {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .background(mColors.background)
-                .padding(mainScaffoldPadding)
+                .padding(mainScaffoldPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-
+            item {
+                HeaderSection(movieDetails = movieDetails)
+            }
         }
     } else {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(mColors.background),
+                .background(mColors.background)
+                .padding(mainScaffoldPadding),
             contentAlignment = Alignment.Center
         ) {
             CircularProgressIndicator()
---
+++ feature/src/main/java/com/example/feature/movie_screen/sections/HeaderSection.kt
@@ -0,0 +1,137 @@
+package com.example.feature.movie_screen.sections
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.core.data.models.movie_detail.MovieDetails
+import com.example.core.ui.theme.mColors
+import com.example.core.ui.theme.mShapes
+import com.example.core.ui.theme.mTypography
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+@Composable
+fun HeaderSection(
+    movieDetails: MovieDetails,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(0.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w500/${movieDetails.posterPath}",
+                contentDescription = null,
+                modifier = Modifier
+                    .weight(0.4f)
+                    .clip(mShapes.medium)
+            )
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.weight(0.6f)
+            ) {
+                Text(
+                    text = movieDetails.title,
+                    style = mTypography.titleLarge
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(
+                            color = mColors.primary,
+                            shape = mShapes.small
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = movieDetails.voteAverage.toString().take(3),
+                        style = mTypography.bodyMedium.copy(
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier.padding(4.dp)
+                    )
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    val genres = movieDetails.genres.joinToString(", ") { it.name }
+                    Text(
+                        text = genres,
+                        style = mTypography.bodyMedium.copy(
+                            color = mColors.secondary
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)
+                    val date = LocalDate.parse(movieDetails.releaseDate, inputFormatter)
+
+                    val outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH)
+                    Text(
+                        text = date.format(outputFormatter),
+                        style = mTypography.bodyMedium.copy(
+                            color = mColors.secondary
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    Text(
+                        text = movieDetails.status,
+                        style = mTypography.bodyMedium.copy(
+                            color = mColors.secondary
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    Text(
+                        text = movieDetails.spokenLanguages.joinToString(", ") { it.name },
+                        style = mTypography.bodyMedium.copy(
+                            color = mColors.secondary
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                    Text(
+                        text = "Adult: ${movieDetails.adult}",
+                        style = mTypography.bodyMedium.copy(
+                            color = mColors.secondary
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+        }
+
+        HorizontalDivider(
+            thickness = 1.dp
+        )
+    }
+}
